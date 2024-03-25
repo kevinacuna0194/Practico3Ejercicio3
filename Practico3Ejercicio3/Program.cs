b4b3e95738628f4657a2fa3d32ab21c4c1530bf3
Kevin Acuña <94419050+kevinacuna0194@users.noreply.github.com>
2024-03-25 20:31:35 -0300

    Agregar archivos de proyecto.

---
+++ Practico3Ejercicio3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Practico3Ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /** Ejercicio 3 
+                Un club deportivo permite el ingreso a la categoría de primera división de todas sus actividades 
+                solo a los mayores de edad. Desea registrar a sus socios en un sistema, de los cuales guarda: 
+                nombre, apellido (ambos distintos de vacío y nulo) y fecha de nacimiento (distinto de nulo).  
+                Cada socio puede practicar hasta 2 deportes. De cada deporte se conoce el nombre (no vacío ni 
+                nulo), si es grupal y cantidad de profesores (debe ser mayor a 0). 
+            **/
+
+
+        }
+    }
+}
